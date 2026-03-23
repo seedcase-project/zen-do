seedcase-project/actions

feat: ✨ implement `zenodo_get_record` (#21)
# Description

This PR adds `zenodo_get_record` and integrates it into the general
flow.

Closes seedcase-project/zen-do#19 closes seedcase-project/zen-do#20 in
spirit

Needs a thorough review.

## Checklist

- [x] Ran `just run-all`

src/actions/zenodo.py
@@ -1,13 +1,129 @@
-def zenodo_record_exists(token: str) -> bool:  # noqa: ARG001
-    """Check whether a Zenodo record already exists for the repository.
+from pathlib import Path
+from typing import Optional
+
+import requests
+from pydantic import BaseModel, ConfigDict
+
+from actions.internals import _filter, _map
+
+
+class ZenodoModel(BaseModel):
+    """Model configuring all Zenodo models."""
+
+    model_config = ConfigDict(extra="allow", frozen=True)
+
+
+class ZenodoCreator(ZenodoModel):
+    """Model representing the creator of a Zenodo record.
+
+    Attributes:
+        name: The name of the creator.
+        affiliation: The affiliation of the creator.
+        orcid: The ORCID of the creator.
+    """
+
+    name: str
+    affiliation: str
+    orcid: str
+
+
+class ZenodoRelatedIdentifier(ZenodoModel):
+    """Model representing an identifier related to a Zenodo record.
+
+    Attributes:
+        identifier: The value of the identifier.
+        relation: The relationship between the record and the other piece of work
+            identified by the identifier.
+        resource_type: The type of the work identified by the identifier.
+        scheme: The scheme followed by the identifier.
+    """
+
+    identifier: str
+    relation: str
+    resource_type: str
+    scheme: Optional[str] = None
+
+
+class ZenodoMetadata(ZenodoModel):
+    """Model representing Zenodo metadata.
+
+    Attributes:
+        title: The title of the record.
+        upload_type: The type of the record.
+        creators: The creators of the record.
+        related_identifiers: Identifiers related to the record.
+    """
+
+    title: str
+    upload_type: str
+    creators: list[ZenodoCreator]
+    related_identifiers: list[ZenodoRelatedIdentifier] = []
+
+
+class ZenodoLinks(ZenodoModel):
+    """Model representing the group of links in Zenodo metadata.
+
+    Attributes:
+        bucket: The file upload link for the record.
+    """
+
+    # Published records cannot receive new file uploads
+    bucket: Optional[str] = None
+
+
+class ZenodoRecord(ZenodoModel):
+    """Model representing a Zenodo record.
+
+    Attributes:
+        id: The ID of the record.
+        metadata: The metadata the record.
+        links: Links to record assets and API endpoints.
+    """
+
+    id: int
+    metadata: ZenodoMetadata
+    links: ZenodoLinks
+
+
+def zenodo_get_record(token: str) -> Optional[ZenodoRecord]:
+    """Gets the Zenodo record for the repository if it exists.
+
+    Gets the repository URL from the `.zenodo.json` file. If one
+    doesn't exist, this function will not work.
 
     Args:
         token: Zenodo API access token.
 
     Returns:
-        True if a record already exists for the repository, False otherwise.
+        The Zenodo record for the repo if it exists, None otherwise.
     """
-    return False
+    repo_url = _get_repo_url()
+
+    response = requests.get(
+        "https://zenodo.org/api/deposit/depositions",
+        headers={"Authorization": f"Bearer {token}"},
+        timeout=10,
+    )
+    response.raise_for_status()
+    records: list[ZenodoRecord] = _map(response.json(), ZenodoRecord.model_validate)
+    matching_records = _filter(
+        records,
+        lambda record: bool(
+            _filter(
+                record.metadata.related_identifiers,
+                lambda id: id.relation == "IsDerivedFrom" and id.identifier == repo_url,
+            )
+        ),
+    )
+
+    if len(matching_records) > 1:
+        raise ValueError(
+            "There are multiple records on Zenodo with the repository URL"
+            f"{repo_url!r} as a 'related identifier'. We only allow one."
+        )
+    if not matching_records:
+        return None
+    return matching_records[0]
 
 
 def zenodo_create_record(token: str) -> None:
@@ -19,10 +135,31 @@ def zenodo_create_record(token: str) -> None:
     ...
 
 
-def zenodo_update_record(token: str) -> None:
+def zenodo_update_record(token: str, id: int) -> None:
     """Publish a new version of an existing Zenodo concept record.
 
     Args:
         token: Zenodo API access token.
+        id: Zenodo record ID.
     """
     ...
+
+
+def _load_zenodo_json() -> ZenodoMetadata:
+    return ZenodoMetadata.model_validate_json(Path(".zenodo.json").read_text())
+
+
+def _get_repo_url() -> str:
+    metadata = _load_zenodo_json()
+    ids = _filter(
+        metadata.related_identifiers,
+        lambda id: id.relation == "IsDerivedFrom",
+    )
+    if len(ids) != 1:
+        raise ValueError(
+            "Expected one (1) `IsDerivedFrom` related identifier in `.zenodo.json` "
+            f"but found {len(ids)}. The identifier is used to find the matching "
+            "Zenodo record and should contain the repository URL."
+        )
+
+    return ids[0].identifier

tests/test_cli.py
@@ -1,11 +1,48 @@
-from pytest import raises
+from pytest import fixture, raises
 
 from actions.cli import app
 
 
-def test_zenodo_publish_runs(monkeypatch):
+@fixture
+def _mock_zenodo_get_record(mocker):
+    return mocker.patch("actions.cli.zenodo_get_record")
+
+
+@fixture
+def _mock_zenodo_update_record(mocker):
+    return mocker.patch("actions.cli.zenodo_update_record")
+
+
+@fixture
+def _mock_zenodo_create_record(mocker):
+    return mocker.patch("actions.cli.zenodo_create_record")
+
+
+def test_zenodo_publish_existing_record(
+    monkeypatch,
+    _mock_zenodo_get_record,
+    _mock_zenodo_create_record,
+    _mock_zenodo_update_record,
+):
     monkeypatch.setenv("ZENODO_TOKEN", "token")
     app("zenodo-publish", result_action="return_value")
+
+    _mock_zenodo_update_record.assert_called_once()
+    _mock_zenodo_create_record.assert_not_called()
+
+
+def test_zenodo_publish_new_record(
+    monkeypatch,
+    _mock_zenodo_get_record,
+    _mock_zenodo_create_record,
+    _mock_zenodo_update_record,
+):
+    monkeypatch.setenv("ZENODO_TOKEN", "token")
+    _mock_zenodo_get_record.return_value = None
+    app("zenodo-publish", result_action="return_value")
+
+    _mock_zenodo_create_record.assert_called_once()
+    _mock_zenodo_update_record.assert_not_called()
 
 
 def test_zenodo_publish_needs_token():

tests/test_zenodo_get_record.py
@@ -1,0 +1,171 @@
+from http import HTTPStatus
+from pathlib import Path
+from typing import Any
+
+from pytest import MonkeyPatch, fixture, raises
+from requests import HTTPError
+from requests_mock import ANY
+
+from actions.zenodo import (
+    ZenodoCreator,
+    ZenodoLinks,
+    ZenodoMetadata,
+    ZenodoRecord,
+    ZenodoRelatedIdentifier,
+    zenodo_get_record,
+)
+
+
+@fixture
+def _zenodo_metadata() -> ZenodoMetadata:
+    return ZenodoMetadata(
+        title="Test Repo",
+        upload_type="poster",
+        creators=[
+            ZenodoCreator(
+                name="Doe, Jane", affiliation="University of Testfalia", orcid="ABC"
+            )
+        ],
+        related_identifiers=[
+            ZenodoRelatedIdentifier(
+                identifier="https://github.com/test-repo",
+                relation="IsDerivedFrom",
+                resource_type="other",
+            )
+        ],
+    )
+
+
+@fixture
+def _zenodo_json(
+    monkeypatch: MonkeyPatch, tmp_path: Path, _zenodo_metadata: ZenodoMetadata
+) -> None:
+    monkeypatch.chdir(tmp_path)
+    (tmp_path / ".zenodo.json").write_text(_zenodo_metadata.model_dump_json())
+
+
+def _make_record(
+    record_id: int = 123, repo_url: str = "https://github.com/test-repo"
+) -> dict[str, Any]:
+    record = ZenodoRecord(
+        id=record_id,
+        metadata=ZenodoMetadata(
+            title="Test Repo",
+            upload_type="poster",
+            creators=[
+                ZenodoCreator(
+                    name="Doe, Jane", affiliation="University of Testfalia", orcid="ABC"
+                )
+            ],
+            related_identifiers=[
+                ZenodoRelatedIdentifier(
+                    identifier=repo_url,
+                    relation="IsDerivedFrom",
+                    resource_type="other",
+                )
+            ],
+        ),
+        links=ZenodoLinks(),
+    )
+    return record.model_dump()
+
+
+def test_returns_record_if_matching_record_has_exactly_one_matching_identifier(
+    requests_mock, _zenodo_json
+):
+    requests_mock.get(
+        url=ANY,
+        json=[
+            _make_record(record_id=12),
+            _make_record(repo_url="https://github.com/another-repo"),
+        ],
+    )
+
+    record = zenodo_get_record("token")
+
+    assert record
+    assert record.id == 12
+
+
+def test_returns_record_if_matching_record_has_at_least_one_matching_identifier(
+    requests_mock, _zenodo_json
+):
+    fetched_record = _make_record(record_id=12)
+    # Duplicate identifier
+    fetched_record["metadata"]["related_identifiers"] *= 2
+    # Different identifier
+    fetched_record["metadata"]["related_identifiers"].append(
+        {
+            "identifier": "https://github.com/another-repo",
+            "relation": "IsDerivedFrom",
+            "resource_type": "other",
+        }
+    )
+    requests_mock.get(url=ANY, json=[fetched_record])
+
+    record = zenodo_get_record("token")
+
+    assert record
+    assert record.id == 12
+
+
+def test_raises_error_if_multiple_matching_records(requests_mock, _zenodo_json):
+    requests_mock.get(
+        url=ANY, json=[_make_record(record_id=12), _make_record(record_id=13)]
+    )
+
+    with raises(ValueError):
+        zenodo_get_record("token")
+
+
+def test_returns_none_if_no_records_on_zenodo(requests_mock, _zenodo_json):
+    requests_mock.get(url=ANY, json=[])
+
+    record = zenodo_get_record("token")
+
+    assert record is None
+
+
+def test_returns_none_if_no_matching_records(requests_mock, _zenodo_json):
+    requests_mock.get(
+        url=ANY, json=[_make_record(repo_url="https://github.com/another-repo")]
+    )
+
+    record = zenodo_get_record("token")
+
+    assert record is None
+
+
+def test_raises_error_if_request_unsuccessful(requests_mock, _zenodo_json):
+    requests_mock.get(url=ANY, status_code=HTTPStatus.BAD_REQUEST)
+
+    with raises(HTTPError):
+        zenodo_get_record("token")
+
+
+def test_raises_error_if_zenodo_json_has_no_repo_url(
+    _zenodo_metadata, monkeypatch, tmp_path
+):
+    monkeypatch.chdir(tmp_path)
+    del _zenodo_metadata.related_identifiers[0]
+    (tmp_path / ".zenodo.json").write_text(_zenodo_metadata.model_dump_json())
+
+    with raises(ValueError):
+        zenodo_get_record("token")
+
+
+def test_raises_error_if_zenodo_json_has_multiple_repo_urls(
+    _zenodo_metadata, monkeypatch, tmp_path
+):
+    monkeypatch.chdir(tmp_path)
+    _zenodo_metadata.related_identifiers.append(
+        ZenodoRelatedIdentifier(
+            identifier="https://github.com/another-repo",
+            relation="IsDerivedFrom",
+            resource_type="other",
+        )
+    )
+    (tmp_path / ".zenodo.json").write_text(_zenodo_metadata.model_dump_json())
+
+    with raises(ValueError):
+        zenodo_get_record("token")

seedcase-project/zen-do

src/zen_do/zenodo.py
@@ -1,13 +1,129 @@
-def zenodo_record_exists(token: str) -> bool:  # noqa: ARG001
-    """Check whether a Zenodo record already exists for the repository.
+from pathlib import Path
+from typing import Optional
+
+import requests
+from pydantic import BaseModel, ConfigDict
+
+from zen_do.internals import _filter, _map
+
+
+class ZenodoModel(BaseModel):
+    """Model configuring all Zenodo models."""
+
+    model_config = ConfigDict(extra="allow", frozen=True)
+
+
+class ZenodoCreator(ZenodoModel):
+    """Model representing the creator of a Zenodo record.
+
+    Attributes:
+        name: The name of the creator.
+        affiliation: The affiliation of the creator.
+        orcid: The ORCID of the creator.
+    """
+
+    name: str
+    affiliation: str
+    orcid: str
+
+
+class ZenodoRelatedIdentifier(ZenodoModel):
+    """Model representing an identifier related to a Zenodo record.
+
+    Attributes:
+        identifier: The value of the identifier.
+        relation: The relationship between the record and the other piece of work
+            identified by the identifier.
+        resource_type: The type of the work identified by the identifier.
+        scheme: The scheme followed by the identifier.
+    """
+
+    identifier: str
+    relation: str
+    resource_type: str
+    scheme: Optional[str] = None
+
+
+class ZenodoMetadata(ZenodoModel):
+    """Model representing Zenodo metadata.
+
+    Attributes:
+        title: The title of the record.
+        upload_type: The type of the record.
+        creators: The creators of the record.
+        related_identifiers: Identifiers related to the record.
+    """
+
+    title: str
+    upload_type: str
+    creators: list[ZenodoCreator]
+    related_identifiers: list[ZenodoRelatedIdentifier] = []
+
+
+class ZenodoLinks(ZenodoModel):
+    """Model representing the group of links in Zenodo metadata.
+
+    Attributes:
+        bucket: The file upload link for the record.
+    """
+
+    # Published records cannot receive new file uploads
+    bucket: Optional[str] = None
+
+
+class ZenodoRecord(ZenodoModel):
+    """Model representing a Zenodo record.
+
+    Attributes:
+        id: The ID of the record.
+        metadata: The metadata the record.
+        links: Links to record assets and API endpoints.
+    """
+
+    id: int
+    metadata: ZenodoMetadata
+    links: ZenodoLinks
+
+
+def zenodo_get_record(token: str) -> Optional[ZenodoRecord]:
+    """Gets the Zenodo record for the repository if it exists.
+
+    Gets the repository URL from the `.zenodo.json` file. If one
+    doesn't exist, this function will not work.
 
     Args:
         token: Zenodo API access token.
 
     Returns:
-        True if a record already exists for the repository, False otherwise.
+        The Zenodo record for the repo if it exists, None otherwise.
     """
-    return False
+    repo_url = _get_repo_url()
+
+    response = requests.get(
+        "https://zenodo.org/api/deposit/depositions",
+        headers={"Authorization": f"Bearer {token}"},
+        timeout=10,
+    )
+    response.raise_for_status()
+    records: list[ZenodoRecord] = _map(response.json(), ZenodoRecord.model_validate)
+    matching_records = _filter(
+        records,
+        lambda record: bool(
+            _filter(
+                record.metadata.related_identifiers,
+                lambda id: id.relation == "IsDerivedFrom" and id.identifier == repo_url,
+            )
+        ),
+    )
+
+    if len(matching_records) > 1:
+        raise ValueError(
+            "There are multiple records on Zenodo with the repository URL"
+            f"{repo_url!r} as a 'related identifier'. We only allow one."
+        )
+    if not matching_records:
+        return None
+    return matching_records[0]
 
 
 def zenodo_create_record(token: str) -> None:
@@ -19,10 +135,31 @@ def zenodo_create_record(token: str) -> None:
     ...
 
 
-def zenodo_update_record(token: str) -> None:
+def zenodo_update_record(token: str, id: int) -> None:
     """Publish a new version of an existing Zenodo concept record.
 
     Args:
         token: Zenodo API access token.
+        id: Zenodo record ID.
     """
     ...
+
+
+def _load_zenodo_json() -> ZenodoMetadata:
+    return ZenodoMetadata.model_validate_json(Path(".zenodo.json").read_text())
+
+
+def _get_repo_url() -> str:
+    metadata = _load_zenodo_json()
+    ids = _filter(
+        metadata.related_identifiers,
+        lambda id: id.relation == "IsDerivedFrom",
+    )
+    if len(ids) != 1:
+        raise ValueError(
+            "Expected one (1) `IsDerivedFrom` related identifier in `.zenodo.json` "
+            f"but found {len(ids)}. The identifier is used to find the matching "
+            "Zenodo record and should contain the repository URL."
+        )
+
+    return ids[0].identifier

tests/test_cli.py
@@ -1,11 +1,48 @@
-from pytest import raises
+from pytest import fixture, raises
 
 from zen_do.cli import app
 
 
-def test_zenodo_publish_runs(monkeypatch):
+@fixture
+def _mock_zenodo_get_record(mocker):
+    return mocker.patch("zen_do.cli.zenodo_get_record")
+
+
+@fixture
+def _mock_zenodo_update_record(mocker):
+    return mocker.patch("zen_do.cli.zenodo_update_record")
+
+
+@fixture
+def _mock_zenodo_create_record(mocker):
+    return mocker.patch("zen_do.cli.zenodo_create_record")
+
+
+def test_zenodo_publish_existing_record(
+    monkeypatch,
+    _mock_zenodo_get_record,
+    _mock_zenodo_create_record,
+    _mock_zenodo_update_record,
+):
     monkeypatch.setenv("ZENODO_TOKEN", "token")
     app("zenodo-publish", result_action="return_value")
+
+    _mock_zenodo_update_record.assert_called_once()
+    _mock_zenodo_create_record.assert_not_called()
+
+
+def test_zenodo_publish_new_record(
+    monkeypatch,
+    _mock_zenodo_get_record,
+    _mock_zenodo_create_record,
+    _mock_zenodo_update_record,
+):
+    monkeypatch.setenv("ZENODO_TOKEN", "token")
+    _mock_zenodo_get_record.return_value = None
+    app("zenodo-publish", result_action="return_value")
+
+    _mock_zenodo_create_record.assert_called_once()
+    _mock_zenodo_update_record.assert_not_called()
 
 
 def test_zenodo_publish_needs_token():

tests/test_zenodo_get_record.py
@@ -1,0 +1,171 @@
+from http import HTTPStatus
+from pathlib import Path
+from typing import Any
+
+from pytest import MonkeyPatch, fixture, raises
+from requests import HTTPError
+from requests_mock import ANY
+
+from zen_do.zenodo import (
+    ZenodoCreator,
+    ZenodoLinks,
+    ZenodoMetadata,
+    ZenodoRecord,
+    ZenodoRelatedIdentifier,
+    zenodo_get_record,
+)
+
+
+@fixture
+def _zenodo_metadata() -> ZenodoMetadata:
+    return ZenodoMetadata(
+        title="Test Repo",
+        upload_type="poster",
+        creators=[
+            ZenodoCreator(
+                name="Doe, Jane", affiliation="University of Testfalia", orcid="ABC"
+            )
+        ],
+        related_identifiers=[
+            ZenodoRelatedIdentifier(
+                identifier="https://github.com/test-repo",
+                relation="IsDerivedFrom",
+                resource_type="other",
+            )
+        ],
+    )
+
+
+@fixture
+def _zenodo_json(
+    monkeypatch: MonkeyPatch, tmp_path: Path, _zenodo_metadata: ZenodoMetadata
+) -> None:
+    monkeypatch.chdir(tmp_path)
+    (tmp_path / ".zenodo.json").write_text(_zenodo_metadata.model_dump_json())
+
+
+def _make_record(
+    record_id: int = 123, repo_url: str = "https://github.com/test-repo"
+) -> dict[str, Any]:
+    record = ZenodoRecord(
+        id=record_id,
+        metadata=ZenodoMetadata(
+            title="Test Repo",
+            upload_type="poster",
+            creators=[
+                ZenodoCreator(
+                    name="Doe, Jane", affiliation="University of Testfalia", orcid="ABC"
+                )
+            ],
+            related_identifiers=[
+                ZenodoRelatedIdentifier(
+                    identifier=repo_url,
+                    relation="IsDerivedFrom",
+                    resource_type="other",
+                )
+            ],
+        ),
+        links=ZenodoLinks(),
+    )
+    return record.model_dump()
+
+
+def test_returns_record_if_matching_record_has_exactly_one_matching_identifier(
+    requests_mock, _zenodo_json
+):
+    requests_mock.get(
+        url=ANY,
+        json=[
+            _make_record(record_id=12),
+            _make_record(repo_url="https://github.com/another-repo"),
+        ],
+    )
+
+    record = zenodo_get_record("token")
+
+    assert record
+    assert record.id == 12
+
+
+def test_returns_record_if_matching_record_has_at_least_one_matching_identifier(
+    requests_mock, _zenodo_json
+):
+    fetched_record = _make_record(record_id=12)
+    # Duplicate identifier
+    fetched_record["metadata"]["related_identifiers"] *= 2
+    # Different identifier
+    fetched_record["metadata"]["related_identifiers"].append(
+        {
+            "identifier": "https://github.com/another-repo",
+            "relation": "IsDerivedFrom",
+            "resource_type": "other",
+        }
+    )
+    requests_mock.get(url=ANY, json=[fetched_record])
+
+    record = zenodo_get_record("token")
+
+    assert record
+    assert record.id == 12
+
+
+def test_raises_error_if_multiple_matching_records(requests_mock, _zenodo_json):
+    requests_mock.get(
+        url=ANY, json=[_make_record(record_id=12), _make_record(record_id=13)]
+    )
+
+    with raises(ValueError):
+        zenodo_get_record("token")
+
+
+def test_returns_none_if_no_records_on_zenodo(requests_mock, _zenodo_json):
+    requests_mock.get(url=ANY, json=[])
+
+    record = zenodo_get_record("token")
+
+    assert record is None
+
+
+def test_returns_none_if_no_matching_records(requests_mock, _zenodo_json):
+    requests_mock.get(
+        url=ANY, json=[_make_record(repo_url="https://github.com/another-repo")]
+    )
+
+    record = zenodo_get_record("token")
+
+    assert record is None
+
+
+def test_raises_error_if_request_unsuccessful(requests_mock, _zenodo_json):
+    requests_mock.get(url=ANY, status_code=HTTPStatus.BAD_REQUEST)
+
+    with raises(HTTPError):
+        zenodo_get_record("token")
+
+
+def test_raises_error_if_zenodo_json_has_no_repo_url(
+    _zenodo_metadata, monkeypatch, tmp_path
+):
+    monkeypatch.chdir(tmp_path)
+    del _zenodo_metadata.related_identifiers[0]
+    (tmp_path / ".zenodo.json").write_text(_zenodo_metadata.model_dump_json())
+
+    with raises(ValueError):
+        zenodo_get_record("token")
+
+
+def test_raises_error_if_zenodo_json_has_multiple_repo_urls(
+    _zenodo_metadata, monkeypatch, tmp_path
+):
+    monkeypatch.chdir(tmp_path)
+    _zenodo_metadata.related_identifiers.append(
+        ZenodoRelatedIdentifier(
+            identifier="https://github.com/another-repo",
+            relation="IsDerivedFrom",
+            resource_type="other",
+        )
+    )
+    (tmp_path / ".zenodo.json").write_text(_zenodo_metadata.model_dump_json())
+
+    with raises(ValueError):
+        zenodo_get_record("token")