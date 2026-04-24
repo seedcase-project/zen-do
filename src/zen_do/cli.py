d76c533afb97693cfec1fbfdbf646b4d7b5f4551
Luke W. Johnston <lwjohnst@gmail.com>
2026-04-24 12:42:57 +0200

    refactor: ⬆️ remove `config_name` arg after update

--- src/zen_do/cli.py
+++ src/zen_do/cli.py
@@ -12,8 +12,6 @@ from zen_do.zenodo import (
 app = setup_cli(
     name="zen-do",
     help="zen-do simplifies interacting with Zenodo for common publishing tasks.",
-    # TODO: Replace/remove this once Soil has been updated so config_name is optional.
-    config_name="",
 )
 
 
