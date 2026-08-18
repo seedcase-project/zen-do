b0301ee37dea5e36ca4544069f1eb46414f7eb6c
martonvago <57952344+martonvago@users.noreply.github.com>
2026-08-18 10:20:37 +0100

    chore: ✨ add `publish` Python CLI command (#133)

--- tests/test_zenodo_get_deposit.py
+++ tests/test_zenodo_get_deposit.py
@@ -1,48 +1,23 @@
-from pathlib import Path
-
-import pydantic
-from pytest import MonkeyPatch, fixture, raises
+from pytest import raises
 
 from zen_do.examples import example_deposit, example_metadata
-from zen_do.internals import _write_metadata
 from zen_do.zenodo_get_deposit import zenodo_get_deposit
 from zen_do.zenodo_metadata import ZenodoRelatedIdentifier
 
 
-@fixture
-def _zenodo_toml(monkeypatch: MonkeyPatch, tmp_path: Path) -> None:
-    monkeypatch.chdir(tmp_path)
-    _write_metadata(example_metadata())
-
-
-def test_returns_deposit_if_matching_deposit_has_exactly_one_matching_identifier(
-    _zenodo_toml,
-):
+def test_returns_deposit_if_matching_deposit_has_exactly_one_matching_identifier():
     deposits = [
         example_deposit(id=12),
         example_deposit(urn="urn:zenodo:my-org:project:poster"),
     ]
 
-    deposit = zenodo_get_deposit(deposits)
+    deposit = zenodo_get_deposit(deposits, example_metadata())
 
     assert deposit
     assert deposit["id"] == 12
 
 
-def test_can_use_non_default_file_location(tmp_path):
-    (tmp_path / "book").mkdir()
-    metadata_path = Path(tmp_path / "book" / ".book.zenodo.toml")
-    _write_metadata(example_metadata(), metadata_path)
-
-    deposit = zenodo_get_deposit([example_deposit(id=12)], metadata_path)
-
-    assert deposit
-    assert deposit["id"] == 12
-
-
-def test_returns_deposit_if_matching_deposit_has_at_least_one_matching_identifier(
-    _zenodo_toml,
-):
+def test_returns_deposit_if_matching_deposit_has_at_least_one_matching_identifier():
     fetched_deposit = example_deposit(id=12)
     fetched_deposit["metadata"]["related_identifiers"].extend(
         [
@@ -64,36 +39,28 @@ def test_returns_deposit_if_matching_deposit_has_at_least_one_matching_identifie
         ]
     )
 
-    deposit = zenodo_get_deposit([fetched_deposit])
+    deposit = zenodo_get_deposit([fetched_deposit], example_metadata())
 
     assert deposit
     assert deposit["id"] == 12
 
 
-def test_raises_error_if_multiple_matching_deposits(_zenodo_toml):
+def test_raises_error_if_multiple_matching_deposits():
     deposits = [example_deposit(id=12), example_deposit(id=13)]
 
     with raises(ValueError):
-        zenodo_get_deposit(deposits)
+        zenodo_get_deposit(deposits, example_metadata())
 
 
-def test_raises_error_if_metadata_file_empty(tmp_path):
-    metadata_path = tmp_path / ".zenodo.toml"
-    metadata_path.touch()
-
-    with raises(pydantic.ValidationError):
-        zenodo_get_deposit([example_deposit()], metadata_path)
-
-
-def test_returns_none_if_no_deposits_on_zenodo(_zenodo_toml):
-    deposit = zenodo_get_deposit([])
+def test_returns_none_if_no_deposits_on_zenodo():
+    deposit = zenodo_get_deposit([], example_metadata())
 
     assert deposit is None
 
 
-def test_returns_none_if_no_matching_deposits(_zenodo_toml):
+def test_returns_none_if_no_matching_deposits():
     deposits = [example_deposit(urn="urn:zenodo:my-org:project:poster")]
 
-    deposit = zenodo_get_deposit(deposits)
+    deposit = zenodo_get_deposit(deposits, example_metadata())
 
     assert deposit is None
