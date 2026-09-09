523c6cdd5d3894ca92671320d0e71b9f445d60c0
Luke W. Johnston <lwjohnst@gmail.com>
2026-09-09 10:22:48 +0200

    refactor: ♻️ convert read and write into Rust

--- src/metadata.rs
+++ src/metadata.rs
@@ -2,6 +2,7 @@
 
 use serde::{Deserialize, Serialize};
 use std::fs;
+use std::path::{PathBuf, Path};
 use std::error::Error;
 
 pub const EXAMPLE_METADATA: &str = r#"
@@ -23,7 +24,7 @@ resource_type = "test"
 // TODO: Include urn property? As in the Python?
 // type for it?
 /// Contains representing Zenodo metadata.
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct Metadata {
     /// The title of the deposit.
     pub title: String,
@@ -41,7 +42,7 @@ pub struct Metadata {
 }
 
 /// The type containing the details of the creator/author of a Zenodo deposit.
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct Creator {
     /// The full name of the creator/author.
     pub name: String,
@@ -56,7 +57,7 @@ pub struct Creator {
 // TODO: Create a check for our URN id, `urn:zenodo:*`, maybe by making a
 // specific type for it?
 /// Model representing an identifier related to a Zenodo deposit.
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct RelatedIdentifier {
     /// The value of the identifier (meaning, the identifier itself).
     pub identifier: String,
@@ -93,20 +94,35 @@ pub struct RelatedIdentifier {
 ///
 /// Outputs a `Box` of Errors if the file couldn't be read correctly or if the
 /// TOML couldn't be parsed.
-pub fn read_metadata(path: &str) -> Result<Metadata, Box<dyn Error>> {
+pub fn read_metadata(path: &Path) -> Result<Metadata, Box<dyn Error>> {
+    // `&Path` is a borrowed immutable reference, since it points to where the file lives.
+
     // `?` means to grab any error types and output them as the `Result`.
     let content: String = fs::read_to_string(path)?;
     let metadata: Metadata = toml::from_str(&content)?;
     Ok(metadata)
 }
 
-// TODO: May have to use another way to write, to preserve comments and order. `toml_edit`?
-pub fn write_metadata(metadata: &Metadata, path: &str) -> Result<(), Box<dyn Error>> {
+/// Writes the Zenodo metadata to the TOML file.
+///
+/// # Arguments:
+///
+/// - `metadata`: The `Metadata` struct that will be converted to TOML and saved to the `path`.
+/// - `path`: The path to the file to save the `metadata`.
+///
+/// # Errors:
+///
+/// Errors to writing to file, such as if there is a problem with the file
+/// itself or where it will be saved in.
+pub fn write_metadata(metadata: &Metadata, path: PathBuf) -> Result<(), Box<dyn Error>> {
+  // `PathBuf` is the owned path to the file, owned to ensure nothing else can
+  // write to it at the same time.
+
+  // TODO: May have to use another way to write, to preserve comments and order. Maybe `toml_edit`?
   let toml_str: String = toml::to_string_pretty(metadata)?;
   fs::write(path, toml_str)?;
   Ok(())
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -116,15 +132,38 @@ mod tests {
     #[test]
     fn test_parse_example() {
         let metadata: Result<Metadata, _> = toml::from_str(EXAMPLE_METADATA);
-        println!("{:?}", metadata);
+        // Uncomment to debug during testing.
+        // println!("{:?}", metadata);
         assert!(metadata.is_ok())
     }
 
     #[test]
-    fn test_write_example() {
-        let metadata: Metadata = toml::from_str(&EXAMPLE_METADATA)?;
+    fn test_reading_metadata() {
+        // TODO: Refactor to write to memory representation of writing, not actual writing (less I/O in tests).
+        use std::io::Write;
 
-        println!("{:?}", metadata);
-        assert!(metadata.is_ok())
+        // `mut` since the file will be written to.
+        let mut file = tempfile::NamedTempFile::new().unwrap();
+        file.write_all(EXAMPLE_METADATA.as_bytes()).unwrap();
+
+        let path = file.path().to_path_buf();
+        let metadata = read_metadata(&path).unwrap();
+        let expected: Metadata = toml::from_str(EXAMPLE_METADATA).unwrap();
+
+        // Compare all because of `PartialEq`.
+        assert_eq!(metadata, expected);
+    }
+
+    #[test]
+    fn test_writing_metadata() {
+        let path = tempfile::NamedTempFile::new()
+            .unwrap()
+            .path()
+            .to_path_buf();
+
+        let example: Metadata = toml::from_str(EXAMPLE_METADATA).unwrap();
+        let write_result = write_metadata(&example, path);
+
+        assert!(write_result.is_ok());
     }
 }
